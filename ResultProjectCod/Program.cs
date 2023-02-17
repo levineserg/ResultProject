591ac95e387b83529b80b206149d00b5c22f0acf
levineserg <levineserg@gmail.com>
2023-02-17 08:53:31 +0400

    Метод ввода с клавиатуры

--- ResultProjectCod/Program.cs
+++ ResultProjectCod/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿void InputArray(string[] arrayInput, int arrayInputLength)
+{
+    for (int i = 0; i < arrayInputLength; i++)
+    {
+        Console.Write($"Введите строку {i}: ");
+        arrayInput[i] = Console.ReadLine();
+    }
+    for (int i = 0; i < arrayInputLength; i++)
+    {
+        Console.Write(arrayInput[i] + "\t");
+    }
+    Console.WriteLine();
+}
