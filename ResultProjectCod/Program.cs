081cc5063ffe7e69f26be38c76c20d0bf49a1b52
levineserg <levineserg@gmail.com>
2023-02-17 09:08:32 +0400

    Решил задачу

--- ResultProjectCod/Program.cs
+++ ResultProjectCod/Program.cs
@@ -11,3 +11,28 @@
     }
     Console.WriteLine();
 }
+void ResultArray(string[] arrayInput, int arrayInputLength, string[] resultArray, int count)
+{
+    for (int i = 0; i < arrayInputLength; i++)
+    {
+        if (arrayInput[i].Length <= 3)
+        {
+            count++;
+            string[] tempArray = new string[count];
+            for (int j = 0; j < count - 1; j++)
+            {
+                tempArray[j] = resultArray[j];
+                resultArray = tempArray;
+            }
+            resultArray[count - 1] = arrayInput[i];
+            Console.Write(resultArray[count - 1] + "\t");
+        }
+    }
+}
+Console.Write("Введите длину массива: ");
+int arrayInputLength = Convert.ToInt32(Console.ReadLine());
+string[] arrayInput = new string[arrayInputLength];
+int count = 0;
+string[] resultArray = new string[count + 1];
+InputArray(arrayInput, arrayInputLength);
+ResultArray(arrayInput, arrayInputLength, resultArray, count);
